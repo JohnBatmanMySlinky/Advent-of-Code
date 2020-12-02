38604c088dce90c9b2b8645301d487a2dfb0b90e
John Myslinski <johnmyslinski@Johns-MacBook-Air.local>
2020-12-01 21:58:14 -0500

    01

---
+++ 01/solution.py
@@ -0,0 +1,20 @@
+with open(r"input.txt","r") as f:
+    dat = [int(x[:-1]) for x in f.readlines()]
+
+# dat = [1721, 979, 366, 299, 675, 1456]
+
+def part1(ls, targ):
+    for i,x in enumerate(ls):
+        for y in ls[i:]:
+            if x + y == targ:
+                return(x*y)
+
+def part2(ls, targ):
+    for i,x in enumerate(ls):
+        for j,y in enumerate(ls[i:]):
+            for z in ls[i+y+1:]:
+                if x + y + z == targ:
+                    return(x*y*z)
+
+print(part1(dat, 2020))
+print(part2(dat, 2020))
