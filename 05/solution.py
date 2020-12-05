2f3cb50515c63644e179af93e1cdb27ca9615673
John Myslinski <johnmyslinski@Johns-MacBook-Air.local>
2020-12-05 10:59:06 -0500

    05 lil clean up

--- 05/solution.py
+++ 05/solution.py
@@ -4,17 +4,19 @@ with open('input.txt', 'r') as f:
 def find_seat_ID(ticket):
     row_idx = [0,127]
     for x in ticket[:7]:
+        row_offset = int((row_idx[1] - row_idx[0]) / 2) + 1
         if x == 'F':
-            row_idx[1] -= int((row_idx[1] - row_idx[0]) / 2) + 1
+            row_idx[1] -= row_offset
         if x == 'B':
-            row_idx[0] += int((row_idx[1] - row_idx[0]) / 2) + 1
+            row_idx[0] += row_offset
 
     col_idx = [0,7]
     for y in ticket[7:]:
+        col_offset = int((col_idx[1] - col_idx[0]) / 2) + 1
         if y == 'L':
-            col_idx[1] -= int((col_idx[1] - col_idx[0]) / 2) + 1
+            col_idx[1] -= col_offset
         if y == 'R':
-            col_idx[0] += int((col_idx[1] - col_idx[0]) / 2) + 1
+            col_idx[0] += col_offset
 
     assert row_idx[0] == row_idx[1]
     assert col_idx[0] == col_idx[1]
