fbb88bf37da379062c974e44361f5d3c6314c47a
John Myslinski <johnmyslinski@Johns-MacBook-Air.local>
2020-12-06 10:34:27 -0500

    06 comments

--- 06/solution.py
+++ 06/solution.py
@@ -7,7 +7,7 @@ print(sum([len(set(list(x.replace('\n','')))) for x in dat]))
 
 # part 2
 dat = [x.split('\n') for x in dat]
-dat[len(dat)-1].remove('')
+dat[len(dat)-1].remove('')           ####  >>:|
 answer2 = 0
 for group in dat:
 	common = set(map(chr, range(97, 123)))
