2dbb9b82c2a83d59751730cd57c62fd40976cb3a
johnmyslinski <jmyslinski@munichre.com>
2021-07-29 11:26:56 +0000

    2019d2p2

--- 2019/02/solution2.py
+++ 2019/02/solution2.py
@@ -1,25 +1,32 @@
 with open(r"input.txt","r") as f:
-    dat = [x.split(",") for x in f.readlines()][0]
-    dat = [int(x) for x in dat]
-    
-dat[1] = 12
-dat[2] = 2
-    
-pos = 0
-while True:
-    op = dat[pos]
-    a,b,c = dat[pos+1], dat[pos+2], dat[pos+3]
-    
-    if op == 1:
-        dat[c] = dat[a] + dat[b]
-        pos += 4
-    elif op == 2:
-        dat[c] = dat[a] * dat[b]
-        pos += 4
-    elif op == 99:
-        print(dat[0])
-        break
-    else:
-        assert 0 > 0 
-    
-        
+    raw = [x.split(",") for x in f.readlines()][0]
+    raw = [int(x) for x in raw]
+
+for x in range(100):
+    for y in range(100):
+
+        dat = raw.copy()
+        dat[1] = x
+        dat[2] = y
+
+        pos = 0
+        while True:
+            op = dat[pos]
+            a,b,c = dat[pos+1], dat[pos+2], dat[pos+3]
+
+            if op == 1:
+                dat[c] = dat[a] + dat[b]
+                pos += 4
+            elif op == 2:
+                dat[c] = dat[a] * dat[b]
+                pos += 4
+            elif op == 99:
+                if x == 12 and y == 2:
+                    print("part1: {}".format(dat[0]))
+                elif dat[0] == 19690720:
+                    print("part2: {}".format(100*x+y))
+
+                break
+            else:
+                assert 0 > 0 
+
