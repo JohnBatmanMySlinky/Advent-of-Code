f05e9e52ac4c2cdd846358d1d8bab5c44dd8f8c1
johnmyslinski <jbmyslin@me.com>
2021-08-07 13:04:54 -0400

    2019d6p2

--- 2019/06/solution6.py
+++ 2019/06/solution6.py
@@ -20,4 +20,42 @@ for each in universe:
         parent = [k for (k,v) in tree.items() if parent in v][0]
     part1_fin += part1
 
-print(part1_fin)
+print('part1: {}'.format(part1_fin))
+
+
+flipped = defaultdict(list)
+root = 'YOU'
+queue = []
+#init flipped
+for k,v in tree.items():
+    if root in v:
+        v.remove(root)
+        flipped[root].append(k)
+        queue.append(k)
+
+i = 0
+while queue:
+    tmp = queue.pop(0)
+    for k,v in tree.items():
+        if tmp == k:
+            flipped[tmp] += v
+            queue += v
+            tree[k] = []
+        elif tmp in v:
+            flipped[tmp].append(k)
+            queue.append(k)
+            v.remove(tmp)
+
+# part 2
+# modified dfs
+visited = set()
+def dfs(visited, flipped, node, goal, depth):
+    if node not in visited:
+        depth += 1
+        if node == goal:
+            print('part2: {}'.format(depth-3))
+        visited.add(node)
+        for neighbor in flipped[node]:
+            dfs(visited, flipped, neighbor, goal, depth)
+
+dfs(visited, flipped, 'YOU', 'SAN', 0)
