ca754c6244dd28e4677402a4f5a254e6c9b2cf60
johnmyslinski <jmyslinski@munichre.com>
2021-09-20 13:54:42 +0000

    2019 d15 p2 refactor

--- 2019/15/solution15.py
+++ 2019/15/solution15.py
@@ -169,28 +169,17 @@ def BFS(visited, queue, graph, node, end):
                 if neighbour not in visited:
                     visited.append(neighbour)
                     queue.append((neighbour, d+1))
+    
+    return d
 
 # finish part 1
 part1 = BFS(visited, queue, path, oxygen, pos)
 print("part1: {}".format(part1))
 
-def BFS2(visited, queue, graph, node):
-    visited.append(node)
-    queue.append((node,1))
-
-    while queue:
-        s, d = queue.pop(0)
-        for neighbour in [generate_next(s,y) for y in range(1,5)]:
-            if neighbour in path:
-                if neighbour not in visited:
-                    visited.append(neighbour)
-                    queue.append((neighbour, d+1))
-                    
-    return d
 
 visited = []
 queue = []
-print("part2: {}".format(BFS2(visited, queue, path, oxygen)))
+print("part2: {}".format(BFS(visited, queue, path, oxygen, (999, 999))))
 
 
 
