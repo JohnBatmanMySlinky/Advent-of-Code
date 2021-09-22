baeedfb8519fc8ad12b14c27f011851c9661a1a4
johnmyslinski <jmyslinski@munichre.com>
2021-09-22 13:57:25 +0000

    wow this tooka while

--- 2019/16/solution16.py
+++ 2019/16/solution16.py
@@ -1,19 +1,58 @@
 with open(r"input.txt","r") as f:
     raw = [int(x) for x in f.readlines()[0]]
     
-
+from array import array
+from copy import deepcopy
+    
+   
 def FFT(signal, phases, base = [0, 1, 0, -1]):
     to_process = signal.copy()
-    for _ in range(phases):
+    for z in range(phases):
+        out = array('b', [])
+        for i in range(1,len(to_process)+1):
+            x = 1
+            work = 0
+            while True:
+                start = ((i*2)*(x-1))+i-1
+                end = min(start + i,len(to_process))
+                if (x+1) % 2 == 0:
+                    mult = 1
+                else:
+                    mult = -1
+                work += mult*sum(to_process[start:end])
+                
+                if end == len(to_process):
+                    break
+                    
+                x += 1
+            out.append(abs(work) % 10)
+        to_process = deepcopy(out)
+    return to_process
+
+print('part1: ' + ''.join([str(x) for x in FFT(raw,100)[:8]]))
+
+
+# thanks excel. 
+# now it is obvious that [0,1,0,-1]*x where x > len(signal) / 2 then it's justa sum of signal[x:]
+# also offset carries through so the raw signal can be offset
+# this is still slow as shit so instead of sum(to_process[i:]) % 10 I can work in reverse
+offset = int(''.join(map(str, raw[:7])))
+new = raw * 10_000
+
+assert offset > len(new)/2
+
+new = new[offset:]
+
+def FFT2(signal, phases):
+    to_process = signal.copy()
+    for z in range(phases):
+        full = sum(to_process)
         out = []
         for i in range(len(to_process)):
-            right = [x for x in base for y in range(i+1)]
-            right = right*(len(signal) // len(right) + 1)
-            right = right[1:]
-            prodsum = sum([x*y for x,y in zip(to_process, right)])
-            keep = int(str(abs(prodsum))[-1])
-            out.append(keep)
-        to_process = out
+            out += [((full % 10) + 10) % 10]
+            full -= to_process[i]
+        to_process = deepcopy(out)
     return to_process
-            
-print('part1: ' + ''.join([str(x) for x in FFT(raw, 100)[:8]]))
+
+print('this takes a minutes or three')
+print("part2: " + "".join(map(str,FFT2(new, 100)[:8])))
