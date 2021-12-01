3eab32019d9a5be6901356160a92abdd8f3ddc85
John M BDAP <jmyslinski@munichre.com>
2021-12-01 14:01:49 +0000

    yeehaw

---
+++ 2021/01/solution01.py
@@ -0,0 +1,17 @@
+with open(r"input.txt","r") as f:
+    dat = [int(x.strip()) for x in f.readlines()]
+
+
+def part1(dat):
+    return sum([y>x for x,y in zip(dat[:-1], dat[1:])])
+print(part1(dat))
+
+
+def part2(dat):
+    counter = 0
+    for x in range(len(dat)-3):
+        counter += sum(dat[x+1:x+3+1]) > sum(dat[x:x+3])
+    return counter
+
+print(part2(dat))
+
