75afe56a4977c5d708257c11c030f06cf6a3b567
johnmyslinski <jbmyslin@me.com>
2021-12-19 15:22:10 -0500

    fuck that rounding bug

--- 2021/18/solution18.py
+++ 2021/18/solution18.py
@@ -24,32 +24,50 @@ def explode(number):
 def split(number):
     for i,(x,d) in enumerate(number):
         if x >= 10:
-            number[i] = (x//2,d+1)
-            number.insert(i+1, (x//2+1,d+1))
+            # FUCK
+            if x//2 == x/2: # FUCK EVEN
+                number[i] = (int(x/2),d+1)
+                number.insert(i+1, (int(x/2),d+1))
+            else: # FUCK ODD
+                number[i] = (x//2,d+1)
+                number.insert(i+1, (x//2+1,d+1))
             return number, True
     return number, False
 
 def part1(dat):
     code = list(parse(dat[0],0))
     for each in dat[1:]:
+
         new = list(parse(each,0))
 
         code += new
         code = [(x,y+1) for x,y in code]
-        print(code)
-        input()
 
-        while max([y for x,y in code]) >= 4:
+        while True:
+            print(code, sum([x+y for x,y in code]))
             code, result = explode(code)
-            if not result:
-                code, result = split(code)
+            if result == True: continue
 
-    while True:
-        print(code)
-        input()
-        code, result = explode(code)
-        if not result:
             code, result = split(code)
+            if result == True: continue
 
-# print(explode(list(parse(dat,0))))
-print(part1(dat))
+            break
+
+    return code
+
+def magnitude(code):
+    print(code)
+    maxdepth = max([y for x,y in code])
+    print("max depth: {}".format(maxdepth))
+    for d in reversed(range(1,maxdepth+1)):
+        for i, (x,y) in enumerate(code):
+            if y == d:
+                code[i] = (x*3+code[i+1][0]*2, y-1)
+                code[i+1] = (-1, -1)
+        code = [(x,y) for x,y in code if x != -1]
+    return code[0][0]
+
+
+# print(part1(dat))
+
+print(magnitude(list(parse(dat,0))))
