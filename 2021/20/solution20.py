1fee4c9a47390675972e1354d5ccc727304594ca
John M BDAP <jmyslinski@munichre.com>
2021-12-24 15:03:51 +0000

    20

---
+++ 2021/20/solution20.py
@@ -0,0 +1,59 @@
+with open('input.txt') as f:
+    raw = [[y.strip() for y in x.split('\n\n')]for x in f.readlines()]
+    decipher = []
+    image = []
+    switch = False
+    for each in raw:
+        if each == ['']:
+            switch = True
+        if switch == False:
+            decipher += each[0]
+        if switch == True and each != ['']:
+            image.append(each[0])
+    decipher = ''.join(decipher)
+
+
+def pad(to_pad, N):
+    for _ in range(N):
+        to_pad.insert(0, '.'*len(to_pad[0]))
+        to_pad.append('.'*len(to_pad[0]))
+        to_pad = ['.' + x + '.' for x in to_pad]
+    return to_pad
+
+
+def update(image, decipher):
+    mapper = {'#': '1', '.': '0'}
+
+    image = pad(image,1)
+
+    new_image = []
+    for y in range(len(image)):
+        new_row = ''
+        for x in range(len(image[0])):
+            binary = ''
+            for (j,k) in [(-1,-1), (0,-1), (1,-1), (-1,0), (0,0), (1,0), (-1,1), (0,1), (1,1)]:
+                if 0 <= x+j <= len(image)-1 and 0 <= y+k <= len(image)-1:
+                    binary += mapper[image[y+k][x+j]]
+                else:
+                    binary += '0'
+            position = int(binary,2)
+            new_row += decipher[position]
+        new_image.append(new_row)
+
+    return new_image
+
+def part1(image, decipher, N):
+    print(image)
+    for _ in range(N):
+        image = update(image, decipher)
+        print(image)
+
+    score = 0
+    for x in image:
+        for y in x:
+            if y == '#':
+                score += 1
+    return score
+
+print(part1(image, decipher, 2))
+        
