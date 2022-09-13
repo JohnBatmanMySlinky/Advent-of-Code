0cec36efffbc6ee28cb9915ea621e39fa1f9890f
Myslinski <n372361b@munichre.com>
2022-09-13 15:19:53 +0000

    2018 3 p2 ex

--- 2018/03/solution03.jl
+++ 2018/03/solution03.jl
@@ -11,8 +11,10 @@ end
 
 function part1(data::Vector{String})
     board = Dict{Tuple{Int64, Int64}, String}()
+    expectation = Dict{String, Int64}()
     for each in data
         box_index, offset_x, offset_y, width_x, width_y = make_nice(each)
+        expectation[box_index] = parse(Int64, width_x) * parse(Int64, width_y)
         for x in 1:parse(Int64, width_x)
             for y in 1:parse(Int64, width_y)
                 idx = (parse(Int64, offset_x) + x, parse(Int64, offset_y) + y)
@@ -24,7 +26,17 @@ function part1(data::Vector{String})
             end
         end
     end
-    return count(==("X"), values(board))
+    p1 = count(==("X"), values(board))
+    p2 = "bad"
+    for each in keys(expectation)
+        if count(==(each), values(board)) == expectation[each]
+            p2 = each
+        end
+    end
+    return p1, p2
 end
 
-print(part1(data))
+p1, p2 = part1(data)
+
+print("part 1: " * string(p1) * "\n")
+print("part 2: " * string(p2))
