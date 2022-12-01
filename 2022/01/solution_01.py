f456216fd3eac3854080b6b30ec9fbe6390f22a4
John Myslinski <johnmyslinski@Johns-MBP-2.fritz.box>
2022-12-01 07:17:10 +0100

    21d1

--- 2022/01/solution_01.py
+++ 2022/01/solution_01.py
@@ -1,5 +1,18 @@
 with open("input.txt") as f:
     dat = [x.strip() for x in f.readlines()]
 
+def do_calorie(dat):
+    cal_list = []
+    calories = 0
+    for each in dat:
+        if each == '':
+            cal_list.append(calories)
+            calories = 0
+            continue
+        else:
+            calories += int(each)
+    return cal_list
+    
 
-print(f"solution 01: {dat}")
+print(f"solution 01: {max(do_calorie(dat))}")
+print(f"solution 02: {sum(list(reversed(sorted(do_calorie(dat))))[:3])}")
