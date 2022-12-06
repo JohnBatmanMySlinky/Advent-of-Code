137b8d40b4c4b10c85e8e172f7763ea0f452ec7a
John Myslinski <johnmyslinski@Johns-MBP-2.fritz.box>
2022-12-06 06:54:53 +0100

    22d6

--- 2022/06/solution06.py
+++ 2022/06/solution06.py
@@ -2,6 +2,8 @@ with open("input.txt") as f:
     data = [x for x in f.readlines()[0]]
 
 def p12(data,N):
+    # could have done a rolling window and pop from tail and append to head
+    # why was part 2 so ez (inb4 day 15)
     for i in range(len(data)-N):
         if len(set(data[i:(i+N)])) == N:
             return i+N
--- 2022/README.md
+++ 2022/README.md
@@ -4,3 +4,4 @@
 3. ez
 4. really ez
 5. parsing took me 85% of my time, rest was ez
+6. really ez
