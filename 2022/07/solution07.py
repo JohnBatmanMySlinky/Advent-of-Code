9fccd52638dd8ffab8862ff926ca358102a98d30
John Myslinski <johnmyslinski@Johns-MBP-2.fritz.box>
2022-12-07 08:19:47 +0100

    22d7 WORKING

---
+++ 2022/07/solution07.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+
+with open("input.txt") as f:
+    data = [x.strip() for x in f.readlines()]
+
+def pop(s):
+    return "||" + "||".join(s.split("||")[1:-1])
+
+def create(data):
+    tree = dict()
+    i = 0
+    key = ""
+    while i-1 < len(data):
+        if data[i] == "$ ls":
+            i += 1
+            while data[i][0] != "$":
+                a,b = data[i].split(' ')
+                if a.isnumeric():
+                    tree[key+"||"+b] = a
+                i += 1
+                if i == len(data):
+                    return tree
+        elif data[i][:4] == "$ cd":
+            a, b, c = data[i].split()
+            if c == "..":
+                key = pop(key)
+            else:
+                key += "||" + c
+            i += 1
+
+def p1(data):
+    tree = create(data)
+    folder_set = defaultdict(int)
+    for k, v in tree.items():
+        for kk in k.split("||")[1:-1]:
+            folder_set[kk] += int(v)
+    winner = 0
+    for k,v in folder_set.items():
+        if v <= 100_000:
+            winner += v
+    return winner
+
+
+print(f"part 1: {p1(data)}")
