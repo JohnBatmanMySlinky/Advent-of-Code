17102a1e83c54c2c1afd51c7076a6aaefb841bf1
Myslinski <n372361b@munichre.com>
2022-12-12 10:04:02 +0000

    I should probably learn to read the prompts better

--- 2022/12/solution_12.py
+++ 2022/12/solution_12.py
@@ -8,12 +8,16 @@ def neighbors(data, p, path):
     n = []
     for i in [1,-1]:
         if (x+i < len(data[0]))&(x+i >= 0):
-            if abs(ord(data[y][x+i]) - ord(data[y][x])) < 2:
+            if (data[y][x+i] == "E")&(data[y][x]=="z"):
+                n.append((x+i,y))
+            if ord(data[y][x+i]) - ord(data[y][x]) <= 1:
                 if (x+i,y) not in path:
                     n.append((x+i,y))
     for i in [1,-1]:
         if (y+i < len(data))&( y+i >= 0):
-            if abs(ord(data[y+i][x]) - ord(data[y][x])) < 2:
+            if (data[y+i][x] == "E")&(data[y][x]=="z"):
+                n.append((x, y+i))
+            if ord(data[y+i][x]) - ord(data[y][x]) <= 1:
                 if (x,y+i) not in path:
                     n.append((x,y+i))
     return n
@@ -38,11 +42,16 @@ def p1(data):
         curr = q.pop(0)
         visited.add(curr)
         for n in neighbors(data, curr, visited):
-            log[n[1]][n[0]] = log[curr[1]][curr[0]] + 1
+            # only update if first visit or better path distance
+            if (log[n[1]][n[0]]==0) | (log[curr[1]][curr[0]] + 1 <= log[n[1]][n[0]]):
+                log[n[1]][n[0]] = log[curr[1]][curr[0]] + 1
             q.append(n)
             visited.add(n)
             
-            # print(q, visited, log)
+            # print(q, visited)
+            # for l in log:
+                # print(l)
+            #     print(" ".join([str(x) for x in l]))
             # input()
 
 
