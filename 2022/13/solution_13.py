edbfaf0b6846fb521a62b80e5314196fb6b6207d
Myslinski <n372361b@munichre.com>
2022-12-13 13:28:49 +0000

    i cant read

--- 2022/13/solution_13.py
+++ 2022/13/solution_13.py
@@ -1,54 +1,53 @@
+import itertools
+from functools import reduce
+
+
 with open("input.txt") as f:
     data = [[eval(y) for y in x.split('\n')] for x in f.read().split('\n\n')]
 
-def compare(x,y, promoted):
-    # if the lists are same length
-    for i in range(max(len(x), len(y))+1):
-        # input()
-        if (i >= len(x)) & (i >= len(y)):
-            print("E")
-            print("True")
-            return True
-        elif (i >= len(x)) & promoted:
-            print("SP")
-            print("TRUE")
-            return False
-        elif (i >= len(y)) & promoted:
-            print("SP")
-            print("True")
-            return True
-        if (i >= len(x)) & (not promoted):
-            print("SP")
-            print("TRUE")
-            return True
-        elif (i >= len(y)) & (not promoted):
-            print("SP")
-            print("True")
-            return False
-        elif isinstance(x[i],list) & isinstance(y[i],list):
-            print("L, L")
-            return compare(x[i], y[i], False)
-        elif isinstance(x[i],list) & (not isinstance(y[i],list)):
-            print("L, NL")
-            return compare(x[i],[y[i]], True)
-        elif (not isinstance(x[i], list)) & isinstance(y[i], list):
-            print("NL", "L")
-            return compare([x[i]], y[i], True)
-        else: 
-            if x[i] > y[i]:
-                print("FALSE")
-                return False
+def compare(x,y):
+    if isinstance(x,int) & isinstance(y,int):
+        if x > y:
+            return -1
+        if x < y:
+            return 1
+        elif x == y:
+            return 0
+    elif isinstance(x,list) & isinstance(y,int):
+        return compare(x,[y])
+    elif isinstance(x,int) & isinstance(y,list):
+        return compare([x],y)
+    elif isinstance(x,list)&isinstance(y,list):
+        z = [compare(xx,yy) for xx,yy in zip(x,y)]
+        for zz in z:
+            if zz != 0:
+                return z
+        return compare(len(x), len(y))
+    else:
+        assert False
+
+from collections.abc import Iterable
+def flatten(xs):
+    for x in xs:
+        if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
+            yield from flatten(x)
+        else:
+            yield x
 
 def p1(data):
     winners = []
-    for i,(a,b) in enumerate(data):
-        print(a,b)
-        FINAL = compare(a,b, False)
-        print(f"FINAL: {FINAL}")
-        if FINAL:
-            print(f"     {i}")
-            winners.append(i+1)
+    for i,x in enumerate(data):
+        deep = compare(*x)
+        flat = flatten([deep])
+        for f in flat:
+            if f == -1:
+                break
+            elif f == 1:
+                winners.append(i+1)
+                break
     return sum(winners)
+
+
 
 
 print(f"part 1: {p1(data)}")
