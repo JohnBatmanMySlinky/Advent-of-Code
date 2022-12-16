58eb88896a46cc22438df1c372928d1cd1ef67b0
John Myslinski <johnmyslinski@Johns-MBP-2.fritz.box>
2022-12-16 21:24:35 +0100

    muahahhahaahhahahahhahahahah hacky solution wins

--- 2022/16/solution_16.py
+++ 2022/16/solution_16.py
@@ -36,7 +36,7 @@ def p1(tree, rate, guess):
                     newstats.append([
                         cumrate + sum([rate[o] for o in opn]),
                         each,
-                        opn.copy(),
+                        opn,
                         pth + [curr]
                     ])
         stats = newstats[:]
@@ -46,3 +46,67 @@ def p1(tree, rate, guess):
     stats.sort()
     return stats[-1][0]
 print(f"part 1: {p1(tree, rate, 500)}")
+
+
+def p2(tree, rate, guess):
+    # BFS
+    # cumrate, currME, currEL, open, path taken so far
+    stats = [[0, 'AA', 'AA', set()]]
+    for m in range(26):
+        # if this requires memoization i quit
+        # can we just toss the shitty ones?
+        if len(stats) > guess:
+            stats.sort()
+            stats = stats[-guess:]
+        newstats = []
+        for cumrate, currME, currEL, opn in stats:            
+            # 1) I open, E open
+            # 2) I move, E move
+            # 3) I move, E open
+
+            # 1)
+            if (currME not in opn) & (rate[currME] > 0):
+                if (currEL not in opn) & (currME != currEL) & (rate[currEL]>0):
+                    newstats.append([
+                        cumrate + sum([rate[o] for o in opn]),
+                        currME,
+                        currEL,
+                        opn | {currME, currEL},
+                    ])
+            # 2) 
+            for eachME in tree[currME]:
+                for eachEL in tree[currEL]:
+                        newstats.append([
+                            cumrate + sum([rate[o] for o in opn]),
+                            eachME,
+                            eachEL,
+                            opn,
+                        ])
+            # 3)
+            for eachME in tree[currME]:
+                if (currEL not in opn) & (rate[currEL]>0):
+                        newstats.append([
+                            cumrate + sum([rate[o] for o in opn]),
+                            eachME,
+                            currEL,
+                            opn | {currEL},
+                        ])
+
+            # 4)
+            for eachEL in tree[currEL]:
+                if (currME not in opn) & (rate[currME]>0):
+                        newstats.append([
+                            cumrate + sum([rate[o] for o in opn]),
+                            currME,
+                            eachEL,
+                            opn | {currME},
+                        ])
+
+
+        stats = newstats[:]
+        # print(stats)
+        # print(m)
+        # input()
+    stats.sort()
+    return stats[-1][0]
+print(f"part 2: {p2(tree, rate, 10_000)}")
