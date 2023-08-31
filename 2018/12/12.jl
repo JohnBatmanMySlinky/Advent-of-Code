769facc4025619bddb74d4ca71a3b142c5f190e2
Myslinski <n372361b@munichre.com>
2023-08-31 09:03:18 +0000

    the plants move right!

--- 2018/12/12.jl
+++ 2018/12/12.jl
@@ -13,9 +13,22 @@ end
 
 function project(input_state::String, T::Int64)
     original_length = length(input_state)
-    pad = "....."
+    lpad_size = 0
     for i in 1:T
-        input_state = pad * input_state * pad
+        if input_state[end-4:end] != "....."
+            rpad = "....."
+        else
+            rpad = ""
+        end
+
+        if input_state[1:5] != "....."
+            lpad = "....."
+            lpad_size += 5
+        else
+            lpad = ""
+        end
+
+        input_state = lpad * input_state * rpad
         new_state = String["." for x in 1:length(input_state)]
         for i in 4:(length(new_state)-3)
             pot = input_state[i-2:i+2]
@@ -23,15 +36,20 @@ function project(input_state::String, T::Int64)
                 new_state[i] = notes[pot]
             end
         end
-        print("$(i): $(input_state)\n")
+        (i % 1 == 0) && print("$(i): $(input_state)\n")
         input_state = join(new_state)
-        print("$(i): $(input_state)\n\n")
+        (i % 1 == 0) && print("$(i): $(input_state)\n")
+        (i % 1 == 0) && print("$(i): $(calc_score(input_state, lpad_size))\n\n")
     end
     
+    return calc_score(input_state, lpad_size)
+end
+
+function calc_score(input_state::String, lpad_size::Int64)::Int64
     score = 0
     for (idx, val) in enumerate(split(input_state, ""))
-        if val ∈ ["#", '#']
-            score += idx - length(pad) * T - 1
+        if val == "#"
+            score += idx - lpad_size - 1
         end
     end
     return score
@@ -40,3 +58,9 @@ end
 input_state, notes = get_data()
 score = project(input_state, 20)
 print("part1: $(score)")
+
+
+# print("\n\n\n")
+# input_state, notes = get_data()
+# score = project(input_state, 50_000_000_000)
+# print("part1: $(score)")
