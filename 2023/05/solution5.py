e5858f09e702fa00191c86037770955d88a45c42
John Myslinski <jbmyslinski@gmail.com>
2023-12-07 16:17:03 -0500

    brute force kinda part 5 mostly working

--- 2023/05/solution5.py
+++ 2023/05/solution5.py
@@ -1,5 +1,9 @@
+import sys
+
+INPUT = sys.argv[1]
+
 def parse():
-    with open("input.txt", "r") as f:
+    with open(INPUT, "r") as f:
         data = [x.strip() for x in f.readlines()]
     seeds = [int(i) for i in data.pop(0).split(": ")[1].split(" ")]
     _ = data.pop(0)
@@ -16,8 +20,7 @@ def parse():
     struct.append(tmp)
     return seeds, struct
 
-def p1():
-    seeds, maps = parse()
+def p1(seeds, maps):
 
     log = {}
     for seed in seeds:
@@ -29,6 +32,8 @@ def p1():
                 if curr in range(inp, inp+r):
                     seedlog.append(curr+dest-inp)
                     found = True
+                if found:
+                    break
             if found == False:
                 seedlog.append(curr)
         log[seed] = seedlog
@@ -42,33 +47,41 @@ def p1():
 
 def p2():
     seeds, maps = parse()
-
     seedranges = [range(x,x+y) for x,y in zip(seeds[::2], seeds[1::2])]
 
-    log = {}
-    for i, seedrange in enumerate(seedranges):
-        print(f"P2: {i+1}/{len(seedranges)}")
-        for seed in seedrange:
-            seedlog = [seed]
-            for chunk in maps:
-                found = False
-                curr = seedlog[-1]
-                for dest, inp, r in chunk:
-                    if curr in range(inp, inp+r):
-                        seedlog.append(curr+dest-inp)
-                        found = True
-                if found == False:
-                    seedlog.append(curr)
-            log[seed] = seedlog
+    upper_bound = 0
+    for seed in seeds[::2]:
+        answer = p1([seed], maps)
+        if (answer > upper_bound) and any([answer in x for x in seedranges]):
+            upper_bound = answer
+
+    # we can use the even number seeds to get an upper bound
+    # then solve in reverse? 500M ok so that's rough but doable?
+    
+    winner = upper_bound
+    for seed in range(0,500_000_000):
+        seedlog = [seed]
+        for chunk in reversed(maps):
+            found = False
+            curr = seedlog[-1]
+            for dest, inp, r in chunk:
+                if curr-dest+inp in range(inp, inp+r):
+                    seedlog.append(curr-dest+inp)
+                    found = True
+                if found:
+                    break
+            if found == False:
+                seedlog.append(curr)
+        if any([seedlog[-1] in seedrange for seedrange in seedranges]):
+            if seedlog[-1] < winner:
+                winner = seedlog[-1]
+    return winner
 
 
-    lowest = 999999999999999999999
-    for k,v in log.items():
-        if v[-1] < lowest:
-            lowest = v[-1]
-    return lowest
 
-a1 = p1()
+
+seeds, maps = parse()
+a1 = p1(seeds, maps)
 print(f"Part 1: {a1}")
 a2 = p2()
 print(f"part 2: {a2}")
