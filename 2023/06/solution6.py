c18178ba9cdc7808999c178556b7b54bd397575b
John Myslinski <jbmyslinski@gmail.com>
2023-12-08 21:52:54 -0500

    huhok

--- 2023/06/solution6.py
+++ 2023/06/solution6.py
@@ -1,4 +1,4 @@
-def parse():
+def parse1():
     with open("input.txt", "r") as f:
         raw = f.readlines()
 
@@ -12,7 +12,7 @@ def parse():
     return data
 
 def p1():
-    time, distance = parse()
+    time, distance = parse1()
 
     log = []
     for t, d in zip(time, distance):
@@ -29,6 +29,35 @@ def p1():
         return log[0] * log[1] * log[2]
     else:
         return log[0] * log[1] * log[2] * log[3]
+    
+def parse2():
+    with open("input.txt", "r") as f:
+        raw = f.readlines()
+
+    data = []
+    for line in raw:
+        for x in reversed(range(1,11)):
+            line = line.replace(" "*x, " "*(x-1))
+        line = line.split(":")
+        data.append([int(x) for x in line[1:]])
+    return data
+
+def p2():
+    time, distance = parse2()
+
+    log = []
+    for t, d in zip(time, distance):
+        combos = 0
+        for x in range(0,t+1):
+            score = x * (t-x)
+            if score > d:
+                combos += 1
+
+        log.append(combos)
+
+    return log[0]
 
 a1 = p1()
 print(f"Part 1: {a1}")
+a2 = p2()
+print(f"Part 2: {a2}")
